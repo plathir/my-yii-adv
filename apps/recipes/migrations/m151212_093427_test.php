80c4f271f3c545bc744300b1df57991a5dbfdddb
plathir <panagiotis@panagiotis-work>
2015-12-14 07:56:23 +0200

    install and uninstall apps

---
+++ apps/recipes/migrations/m151212_093427_test.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151212_093427_test extends Migration
+{
+    public function up()
+    {
+        $this->createTable('recipes', [
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+            'content' => Schema::TYPE_TEXT,
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('recipes');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
